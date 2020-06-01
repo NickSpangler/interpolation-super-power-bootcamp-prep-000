e7248b867231452b57ec57ea6934a7b08b89ef01
Nick Spangler <spangleyman@mac.com>
2020-06-01 21:40:58 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,8 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  put "R: #{colors[0]}"
+  put "O: #{colors[1]}"
+  put "Y: #{colors[2]}"
+  put "G: #{colors[3]}"
+  put "B: #{colors[0]}"
+end
